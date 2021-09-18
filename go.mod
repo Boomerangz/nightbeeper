97166ad6a11f8b681de0e6a9aa726d76f78b4a8c
Igor Zygin <izygin@ozon.ru>
2021-09-18 15:04:44 +0300

    initial

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/Boomerangz/nightbeeper
+
+go 1.17
+
+require (
+	github.com/faiface/beep v1.1.0
+	github.com/imroc/req v0.3.0
+)
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.0 // indirect
+	github.com/hajimehoshi/oto v0.7.1 // indirect
+	github.com/jfreymuth/oggvorbis v1.0.1 // indirect
+	github.com/jfreymuth/vorbis v1.0.0 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/mattn/go-tty v0.0.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
+	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
+	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
+)
---
+++ main.go
@@ -0,0 +1,267 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/imroc/req"
+	"github.com/mattn/go-tty"
+	"io/ioutil"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type SugarLevel struct {
+	ID         string  `json:"_id"`
+	Device     string  `json:"device"`
+	Date       int64   `json:"date"`
+	DateString string  `json:"dateString"`
+	SugarLevel int     `json:"sgv"`
+	Delta      float64 `json:"delta"`
+	Direction  string  `json:"direction"`
+	Type       string  `json:"type"`
+	Filtered   int     `json:"filtered"`
+	Unfiltered int     `json:"unfiltered"`
+	Rssi       int     `json:"rssi"`
+	Noise      int     `json:"noise"`
+	SysTime    string  `json:"sysTime"`
+}
+
+var sugarDangerous bool
+var sugarLevel float64
+var snoozedTill time.Time
+var ctrl beep.Ctrl
+var measureTime time.Time
+
+//go:embed assets/*.mp3
+var f embed.FS
+//go:embed config.txt
+var configFS embed.FS
+
+func playSugar() {
+
+	strs := strings.Split(strconv.FormatFloat(sugarLevel, 'f', 1, 64), ".")
+	f1, err := f.Open("assets/" + strs[0] + ".mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	f2, err := f.Open("assets/dot.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f3, err := f.Open("assets/" + strs[1] + ".mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streamer1, _, err := mp3.Decode(f1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer1.Close()
+
+	streamer2, _, err := mp3.Decode(f2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer2.Close()
+
+	streamer3, _, err := mp3.Decode(f3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer3.Close()
+
+	done := make(chan bool)
+
+	speaker.Lock()
+	paused := ctrl.Paused
+	ctrl.Paused = true
+	speaker.Unlock()
+
+	speaker.Play(beep.Seq(streamer1, streamer2, streamer3, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
+	speaker.Lock()
+	ctrl.Paused = paused
+	speaker.Unlock()
+}
+
+func main() {
+	fmt.Println("asdasd")
+	f, err := f.Open("assets/ring.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	buffer := beep.NewBuffer(format)
+	buffer.Append(streamer)
+	streamer.Close()
+
+	bufferStreamer := buffer.Streamer(0, buffer.Len())
+	ctrl = beep.Ctrl{Streamer: beep.Loop(-1, bufferStreamer), Paused: true}
+	speaker.Play(&ctrl)
+
+	//go runTelegramBot()
+	go refreshSugarLevel()
+
+	tty, err := tty.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tty.Close()
+
+	for {
+		r, err := tty.ReadRune()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Key press => " + string(r))
+		switch r {
+		case '1':
+			{
+				delta := time.Second * 5
+				snoozedTill = time.Now().Add(delta)
+				fmt.Println("snoozed for 5 seconds")
+				startSpeakerIfNeeded()
+				go func() {
+					time.Sleep(delta)
+					startSpeakerIfNeeded()
+				}()
+				bufferStreamer.Seek(0)
+			}
+		case '2':
+			{
+				delta := time.Second * 15
+				snoozedTill = time.Now().Add(delta)
+				fmt.Println("snoozed for 15 seconds")
+				startSpeakerIfNeeded()
+				go func() {
+					time.Sleep(delta)
+					startSpeakerIfNeeded()
+				}()
+				bufferStreamer.Seek(0)
+			}
+		case '3':
+			{
+				playSugar()
+			}
+		}
+
+	}
+}
+
+func refreshSugarLevel() {
+	for {
+		time.Sleep(10 * time.Second)
+		resp, err := req.Get(os.Getenv("NIGHTSCOUT") + "/api/v1/entries/current.json")
+		if err != nil {
+			log.Println("err", err.Error())
+			continue
+		}
+		var levels []SugarLevel
+		err = resp.ToJSON(&levels)
+		if err != nil {
+			log.Println("err", err.Error())
+			continue
+		}
+		if len(levels) == 0 {
+			log.Println("empty array")
+			continue
+		}
+
+		level := levels[0]
+		sugarLevel = float64(level.SugarLevel) * 0.0555
+		measureTime, _ = time.Parse("2006-01-02T15:04:05-0700", level.DateString)
+		delta := level.Delta * 0.0555
+		fmt.Println(math.Round(sugarLevel*10)/10, math.Round(delta*10)/10, measureTime, snoozedTill, sugarDangerous)
+		startSpeakerIfNeeded()
+	}
+}
+
+func getMinMax() (float64, float64) {
+	var min, max float64
+	b, err := ioutil.ReadFile("config.txt") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+		b, err = configFS.ReadFile("config.txt")
+		if err != nil {
+			fmt.Print(err)
+			return 5, 9
+		}
+	}
+
+	for _, s := range strings.Split(string(b), "\n") {
+		i, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			fmt.Print(err)
+		}
+		if i < min {
+			min = i
+		}
+		if i > max {
+			max = i
+		}
+	}
+	return min, max
+}
+
+func startSpeakerIfNeeded() {
+	min, max := getMinMax()
+	sugarDangerous = sugarLevel > max || sugarLevel < min || time.Since(measureTime) > time.Minute*10
+	speaker.Lock()
+	ctrl.Paused = !(sugarDangerous && time.Now().After(snoozedTill))
+	speaker.Unlock()
+}
+
+
+func runTelegramBot() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TGTOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		d1 := []byte(update.Message.Text)
+    	err := os.WriteFile("config.txt", d1, 0644)
+		if err != nil {
+			log.Panic(err)
+		}
+		
+		// msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		// msg.ReplyToMessageID = update.Message.MessageID
+
+		// bot.Send(msg)
+	}
+}
